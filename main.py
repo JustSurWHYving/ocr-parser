9b03e6990c55ac8f859fcdca9193b0003d206727
Ashish Prajapati <ashishanshprajapati@gmail.com>
2025-03-30 01:06:38 +0530

    Added a choice of services

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ It loads configuration settings and initiates the document analysis process.
 """
 
 # Import configuration loading utility from utils.config module
-from utils.azure_config import load_config
+from utils.azure_config import load_config_azure
+from utils.google_config import load_config_google
 # Import document analysis function from utils.analyzer module
 from utils.azure_analyzer import analyze_document_read
+from utils.google_analyzer import process_document_ocr
 
-def main():
+def main(service):
     """
     Main function that orchestrates the document analysis workflow.
     
@@ -17,16 +19,29 @@ def main():
     2. Extracts necessary configuration values
     3. Initiates document analysis
     """
-    # Load configuration settings from .env file
-    config = load_config()
     
-    # Extract required configuration values from the loaded config
-    endpoint = config["endpoint"]  # API endpoint for the analysis service
-    key = config["key"]           # Authentication key for the service
-    file_path = config["file_path"]  # Path to the document to be analyzed
+    if service == "azure":
+        config = load_config_azure()
+
+        # Extract required configuration values from the loaded config
+        endpoint = config["endpoint"]  # API endpoint for the analysis service
+        key = config["key"]           # Authentication key for the service
+        file_path = config["file_path"]  # Path to the document to be analyzed
+        
+        # Analyze the document using the provided configuration
+        result = analyze_document_read(endpoint, key, file_path=file_path, save_output=True) 
+
+    elif service == "google":
+        config = load_config_google()
+    else:
+        print("Invalid service selection. Please choose 'azure' or 'google'.")
+        return
     
-    # Analyze the document using the provided configuration
-    result = analyze_document_read(endpoint, key, file_path=file_path, save_output=True)
+
 
 if __name__ == "__main__":
-    main()
+    print(f"Choose service to use for OCR:")
+    print("azure" + "/" + "google")
+    service = input("Enter the number of the service you want to use: ")
+
+    main(service)
